bce35d14d2ee9a4127a9a893331d0d9b7fbf2fca
Cayman Freeman <freemancayman@gmail.com>
2024-06-29 11:45:16 -0500

    Fix Replay Notification

--- start_replay.sh
+++ start_replay.sh
@@ -3,4 +3,5 @@
 VIDEO_PATH="$HOME/Videos"
 
 mkdir -p "$VIDEO_PATH"
-flatpak run --command=gpu-screen-recorder com.dec05eba.gpu_screen_recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -c mkv -r 90 -k h265 -o "$VIDEO_PATH" && sleep 0.5 && notify-send -t 1500 -u normal -- "GPU Screen Recorder" "Replay started"
+notify-send -t 1500 -u normal -- "GPU Screen Recorder" "Replay started"
+flatpak run --command=gpu-screen-recorder com.dec05eba.gpu_screen_recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -c mkv -r 90 -k h265 -o "$VIDEO_PATH"
