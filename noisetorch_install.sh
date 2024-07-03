b76a0db20fb096fe3d3ad4b1adcee69802d835c9
Cayman Freeman <138227022+CaymanFreeman@users.noreply.github.com>
2024-07-03 14:19:51 -0500

    Delete noisetorch_install.sh

--- noisetorch_install.sh
+++
@@ -1,8 +0,0 @@
-sudo -v
-
-LATEST_TAG=$(curl -s "https://api.github.com/repos/noisetorch/NoiseTorch/releases/latest" | grep 'tag_name' | cut -d\" -f4)
-ASSET_NAME="NoiseTorch_x64_$LATEST_TAG.tgz"
-DOWNLOAD_URL="https://github.com/noisetorch/NoiseTorch/releases/download/$LATEST_TAG/$ASSET_NAME"
-cd "$HOME/Downloads" && curl -JLO $DOWNLOAD_URL && tar -C $HOME -h -xzf $ASSET_NAME && rm $ASSET_NAME && cd
-gtk-update-icon-cache
-sudo setcap 'CAP_SYS_RESOURCE=+ep' "$HOME/.local/bin/noisetorch"
